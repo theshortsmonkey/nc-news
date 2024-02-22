c04e7eca2966f466e424b584e1aa2c6a62d40bed
theshortsmonkey <j.horsley72@gmail.com>
2024-02-22 11:00:10 +0000

    created and error handled POST /api/topics endpoint

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,17 @@
-const { selectTopics } = require("../models/topics.models")
+const { selectTopics, insertTopic } = require("../models/topics.models")
 
 exports.getTopics = (req,res,next) => {
   return selectTopics()
   .then((topics) => {
     return res.status(200).send({topics})
+  })
+  .catch(next)
+}
+
+exports.postTopic = (req,res,next) => {
+  return insertTopic(req.body)
+  .then((postedTopic) => {
+    res.status(201).send({postedTopic})
   })
   .catch(next)
 }
